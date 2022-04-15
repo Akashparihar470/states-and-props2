da0d4add0d9c682da9f796d8fa4d1286f36ad245
Akash Parihar <96344598+Akashparihar470@users.noreply.github.com>
2022-04-15 16:46:56 +0530

    update

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,7 +6,7 @@ export const Counter = () =>{
    const [count,setCount] = useState(5)
   
     return(
-        <div className="counter"> <h1>Counter:{count}</h1>
+        <div className="counter" > <h1 className={ count%2 === 0 ? "green" : "red"}>Counter:{count}</h1>
           <div>
             <button onClick={()=>{setCount(count+1)}}>Increment</button>
             <button onClick={()=>{setCount(count-1)}}>Decrement</button>
